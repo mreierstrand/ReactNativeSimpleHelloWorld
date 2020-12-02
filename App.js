55426d8b9e11e626f60821c49ac9416aab22a4fd
oliv1115 <48084927+oliv1115@users.noreply.github.com>
2020-12-02 13:07:50 +0100

    View in View in View

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+  
+  render(){
+
+    return (
+
+      <View style={styles.HelloWorldView}>
+      <StatusBar hidden />
+        <Text style={styles.HelloWorldText}>Første View</Text>
+          <View  style={{backgroundColor:'yellow',margin:5, width: 350, height: 450, alignItems: 'center'}}>
+           <Text style={{fontSize: 30}}>Andet View</Text>
+            <View style={{backgroundColor:'green',margin:5, width: 330, height: 395,alignItems: 'center'}}>
+              <Text style={{fontSize: 30}}>Tredje View</Text>
+            </View>
+          </View>
+      </View>
+
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  HelloWorldView: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  HelloWorldText: {
+    fontSize: 50,
+    fontWeight: 'bold',
+  },
 });
+
+
